b6a151b27ad09208a962f2b988ae15761287fb3e
Shashwat Mishra <11258035+secrethash@users.noreply.github.com>
2023-01-02 23:31:40 +0530

    Add: Deployment Scripts

---
+++ server_deploy.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+set -e
+
+echo "Deploying application ..."
+
+# Enter maintenance mode
+(php artisan down) || true
+    # Update codebase
+    git fetch origin deploy
+    git reset --hard origin/deploy
+
+    # Install dependencies based on lock file
+    # composer update --no-interaction --prefer-dist --optimize-autoloader
+    composer install --no-interaction --prefer-dist --optimize-autoloader
+
+    # Migrate database
+    php artisan migrate --force
+
+    # Clear cache
+    php artisan optimize:clear
+    php artisan optimize
+
+    # Note: If you're using queue workers, this is the place to restart them.
+    php artisan queue:restart
+
+    # Reload PHP to update opcache
+    # echo "" | sudo -S service php8.1-fpm reload
+# Exit maintenance mode
+php artisan up
+
+echo "Application deployed!"
